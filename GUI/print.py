851668f1787127a2b388d56f4e278d4603f64868
Shimada <mylinux1204@gmail.com>
2015-06-25 00:31:24 +0900

    Full screen mode and window mode

--- GUI/print.py
+++ GUI/print.py
@@ -12,14 +12,12 @@ class Game(object):
 	def __init__(self):
 		title = 'Game!!'
 		pygame.display.set_caption(title)
-		# setup font
-		#self.font_size = 18 # default font size
-		#self.f_serif = pygame.font.Font("IPAexfont/ipaexm.ttf", self.font_size)
-		#self.f_sans_serif = pygame.font.Font("IPAexfont/ipaexg.ttf", self.font_size)
 		self.pos_y = 0
 
-	def main(self, screen):
-		self.screen = screen
+	def main(self, screen_size):
+		f_screen_full = False
+		clock = pygame.time.Clock()
+		self.screen = pygame.display.set_mode((640, 480))
 		self.screen.fill((255, 255, 255))
 		self.print_font("Welcome to Gaming world!")
 		self.file_story = codecs.open("story.txt", "r", "utf-8")
@@ -27,6 +25,8 @@ class Game(object):
 		self.print_title_screen()
 		
 		while True:
+			clock.tick(60) # 60 fps
+			pygame.display.update()
 			for event in pygame.event.get():
 				if event.type == QUIT:
 					#sys.exit()
@@ -35,6 +35,14 @@ class Game(object):
 						event.key == pygame.K_ESCAPE:
 					#sys.exit()
 					return
+				elif event.type == pygame.KEYDOWN and \
+						event.key == pygame.K_F2:
+					f_screen_full = not f_screen_full
+					if f_screen_full == True:
+						self.screen = pygame.display.set_mode(screen_size, FULLSCREEN)
+					else:
+						self.screen = pygame.display.set_mode((640, 480))
+					#self.screen.fill((255, 255, 255))
 				elif event.type == pygame.KEYDOWN and \
 						event.key == pygame.K_RETURN:
 					self.puts_message()
@@ -59,13 +67,18 @@ class Game(object):
 		self.screen.fill((0, 0, 0))
 		self.print_font("Game start!", (255, 0, 0), 48, (10, 100, 10))
 
+def get_screen_size():
+	user32 = ctypes.windll.user32
+	screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+	return screensize
+
 ### ----------- main ----------- ###
 if __name__ == '__main__':
-	screen_size = (640, 480)
+	#screen_size = (640, 480)
 	pygame.init()
 	#screen = pygame.display.set_mode(screen_size)
-	screen = pygame.display.set_mode(screen_size, FULLSCREEN)
-	width, height = screen.get_size()
-	print width #+ " : " + height
+	f_screen_size = get_screen_size()
+	#fullscreen = pygame.display.set_mode(f_screen_size, FULLSCREEN)
 	game = Game()
-	game.main(screen)
+	#game.main(screen, fullscreen)
+	game.main(f_screen_size)
